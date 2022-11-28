6abb560f21092a45f890cc2c368196414a39c192
Radosław Siek <106437063+Radexman@users.noreply.github.com>
2022-11-28 12:23:52 +0100

    Further objects
    Objects made with constructors and literal notation

--- app.js
+++ app.js
@@ -2,15 +2,17 @@
 // Functions can take arguments and parameters do make calculations
 // If we want to use function output we need to return it's value
 
-function calculateValues(width, height, depth) {
+function calculateSizes(width, height, depth) {
     let area = width * height;
     let volume = width * height * depth;
     let sizes = [area, volume];
     return sizes;
 }
 
-let areaOne = calculateValues(10, 5, 2)[0];
-let volumeOne = calculateValues(10, 5, 2)[1];
+let areaOne = calculateSizes(10, 5, 2)[0];
+let volumeOne = calculateSizes(10, 5, 2)[1];
+console.log(areaOne);
+console.log(volumeOne);
 
 // ============================================================ Objects ================================================================ //
 // Objects are groupings of variables and functions
@@ -26,9 +28,9 @@ const hotelAvialableRoomsEl = document.querySelector('#hotelAvialableRoomsEl');
 
 // Object Hotel
 const hotel = {
-    name: 'Apart',
+    name: 'Quay',
     rooms: 150,
-    booked: 123,
+    booked: 132,
     gym: true,
     pool: false,
     roomTypes: [
@@ -38,23 +40,30 @@ const hotel = {
         'suite'
     ],
     checkAvialability: function() {
-        let avialableRooms = hotel.rooms - hotel.booked;
-        return avialableRooms;
+        return hotel.rooms - hotel.booked;
     }
-}
+};
 
-hotel.name = '';
+function Car(brand, power, year, ABS){
+    this.brand = brand;
+    this.power = power;
+    this.year = year;
+    this.ABS = ABS;
+};
+
+let BMW = new Car('BMW', '340KM', 1996, false);
+let Bugatti = new Car('Bugatti', '900KM', 2016, true);
 
 // Hotel Object Proprieties
 let hotelName = hotel.name;
 let hotelRooms = hotel.rooms;
 let hotelBookedRooms = hotel.booked;
 let hotelRoomTypes = hotel.roomTypes;
-let aviability = hotel.checkAvialability();
+let aviability = 'Rooms avialable in ' + hotel.name + ': ';
 
 // Displaying Proprieties in DOM
 hotelNameEl.textContent+= hotelName;
 hotelRoomsEl.textContent+= hotelRooms;
 hotelBookedRoomsEl.textContent+= hotelBookedRooms;
 hotelRoomTypesEl.textContent+= hotelRoomTypes;
-hotelAvialableRoomsEl.textContent+= aviability;
+hotelAvialableRoomsEl.textContent = aviability + hotel.checkAvialability();
